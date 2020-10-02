89312ac96645eefc8f7dcdb4a01c2853bc7729c4
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-10-02 08:00:18 +0700

    Gorilla Session Cookie

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gosimplemux
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
+)
--- main/app.go
+++ main/app.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"gosimplemux/appHttpResponse"
 	"gosimplemux/appStatus"
 	"log"
@@ -17,23 +18,58 @@ type User struct {
 }
 
 func homeRoute(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "app-cookie")
 	user1 := User{
 		Id:        "123",
 		FirstName: "Edi",
 		LastName:  "Uchida",
 	}
+	if isAuth, ok := session.Values["authenticated"].(bool); !isAuth || !ok {
+		http.Error(w, "", http.StatusUnauthorized)
+		return
+	}
+
 	responder.Data(w, appStatus.Success, appStatus.StatusText(appStatus.Success), user1)
 }
 
+func authRoute(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "app-cookie")
+	store.Options = &sessions.Options{
+		MaxAge:   60 * 1,
+		HttpOnly: true,
+	}
+	session.Values["authenticated"] = true
+	err := session.Save(r, w)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+func authLogoutRoute(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "app-cookie")
+	session.Values["authenticated"] = false
+	session.Options.MaxAge = -1
+	err := session.Save(r, w)
+	fmt.Println(session.Values)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 var responder = appHttpResponse.NewJSONResponder()
+var store = sessions.NewCookieStore([]byte("rahasia..."))
 
 func main() {
 	hostPtr := flag.String("host", "localhost", "Listening on host")
 	portPtr := flag.String("port", "6969", "Listening on port")
 	flag.Parse()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/user", homeRoute)
 	r.HandleFunc("/user/{id}", homeRoute)
+
+	r.HandleFunc("/login", authRoute)
+	r.HandleFunc("/logout", authLogoutRoute)
+
 	h := fmt.Sprintf("%s:%s", *hostPtr, *portPtr)
 	log.Println("Listening on", h)
 	err := http.ListenAndServe(h, r)
